807b21ebf843ca02b7b0488c86261ed871efa9ba
Hannah Young <hannahyoung@Hannahs-Personal-MacBook.local>
2022-12-29 19:10:45 -0600

    JobPrep initial commit

--- src/MergeNames.java
+++ src/MergeNames.java
@@ -1,5 +1,22 @@
-public class Main {
+import java.util.HashSet;
+import java.util.Set;
+
+public class MergeNames {
+    public static String[] uniqueNames(String[] names1, String[] names2) {
+        Set<String> uniqNames = new HashSet<>();
+        for (String name : names1) {
+            uniqNames.add(name);
+        }
+        for (String name : names2) {
+            uniqNames.add(name);
+        }
+        String[] output = new String[uniqNames.size()];
+        return uniqNames.toArray(output);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] names1 = new String[] {"Ava", "Emma", "Olivia"};
+        String[] names2 = new String[] {"Olivia", "Sophia", "Emma"};
+        System.out.println(String.join(", ", MergeNames.uniqueNames(names1, names2))); // should print Ava, Emma, Olivia, Sophia
     }
 }
--- src/Node.java
+++ src/Node.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Node {
+class Node {
+    public int value;
+    public Node left, right;
+
+    public Node(int value, Node left, Node right) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class BinarySearchTree {
+    public static boolean contains(Node root, int value) {
+//        boolean found;
+//        if (root == null) {
+//            found = false;
+//        } else if (root.value > value && contains(root.left, value)) {
+//            found = true;
+//        } else if (root.value < value && contains(root.right, value)) {
+//            found = true;
+//        } else {
+//            found = true;
+//        }
+//        return found;
+        if (root.value == value) return true;
+        if (root.value < value && root.right != null && contains(root.right, value)) return true;
+        if (root.value > value && root.left != null && contains(root.left,value)) return true;
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Node n1 = new Node(1, null, null);
+        Node n3 = new Node(3, null, null);
+        Node n2 = new Node(2, n1, n3);
+
+        System.out.println(contains(n2, 3));
+    }
 }
